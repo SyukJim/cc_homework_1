2719fb256ea4ad34c0a934d6561af10b74ddcb8f
SyukJim <98511989+SyukJim@users.noreply.github.com>
2022-02-04 17:59:06 +0800

    Create cc_clean_code.cs

---
+++ cc_clean_code.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace cc_clean_code
+{
+
+  class Program
+  {
+  
+    static void Main(string[] args)
+    {
+    
+      Console.WriteLine("Clean code");
+    
+    }
+  
+  }
+
+}
